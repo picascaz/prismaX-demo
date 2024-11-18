ea601c104e3d0be76096897252d7c36a8ddc9d72
Yozora <yozoraworks@users.noreply.github.com>
2024-11-18 16:39:44 +0800

    new models

--- app/page.js
+++ app/page.js
@@ -10,7 +10,6 @@ export default function RootLayout() {
   return (
     <>
       <Navbar />
-      <Intro />
       <ModelPage />
       <Third />
       <div style={{
--- components/ModelPage/ModelPage.js
+++ components/ModelPage/ModelPage.js
@@ -3,11 +3,10 @@
 import React from "react";
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
-import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
-import Spline from "@splinetool/react-spline";
 
 const gltfUrl = 'model.glb'
 let currentgltf = null
+let bubbleContainer = null
 const videoTextureURL = 'pol.mp4'
 
 export default function ModelPage() {
@@ -23,8 +22,8 @@ export default function ModelPage() {
     const width = container.clientWidth,
       height = container.clientHeight
     renderer.setSize(width, height)
-    const camera = new THREE.PerspectiveCamera(70, width / height, 0.5, 1000);
-    camera.position.set(0, 0.3, 6);
+    const camera = new THREE.PerspectiveCamera(30, width / height, 0.5, 200);
+    camera.position.set(0, 0.2, 15);
 
     //on window resize
     window.addEventListener('resize', () => {
@@ -35,13 +34,58 @@ export default function ModelPage() {
       camera.updateProjectionMatrix()
     })
 
-    const light = new THREE.DirectionalLight(0xffffff, 5);
-    light.position.set(0, 10, 0);
-    scene.add(light);
+    //load hdri
+    const pmremGenerator = new THREE.PMREMGenerator(renderer)
+    pmremGenerator.compileEquirectangularShader()
 
-    //ambient light
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.01);
-    scene.add(ambientLight);
+    //load jpg as texture
+    const textureLoader = new THREE.TextureLoader()
+    textureLoader.load('/hdri.jpg', function (texture) {
+      const envMap = pmremGenerator.fromEquirectangular(texture).texture
+      scene.environment = envMap
+
+      pmremGenerator.dispose()
+    })
+
+
+    /*
+    const hdriLoader = new RGBELoader()
+    let hdri = null
+    hdriLoader.load('/hdri.hdr', function (texture) {
+      hdri = pmremGenerator.fromEquirectangular(texture).texture
+      texture.dispose()
+      scene.environment = hdri
+      //scene.background = hdri
+
+      pmremGenerator.dispose()
+    })
+       */
+
+    //ambient
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.15)
+    scene.add(ambientLight)
+
+    //center spotlight
+    const pointLght = new THREE.PointLight(0xffffff, 200)
+    pointLght.position.set(0, -1.3, 0.55)
+    //radius
+    pointLght.distance = 1.5
+    scene.add(pointLght)
+
+    //center spotlight2
+    const pointLght2 = new THREE.PointLight(0xffffff, 20)
+    pointLght2.position.set(-2.5, -1.3, 1)
+    //radius
+    pointLght2.distance = 3
+    scene.add(pointLght2)
+
+
+    //center spotlight3
+    const pointLght3 = new THREE.PointLight(0xffffff, 20)
+    pointLght3.position.set(0.2, 1, 0.3)
+    //radius
+    pointLght3.distance = 1
+    scene.add(pointLght3)
 
     const loader = new GLTFLoader()
 
@@ -50,6 +94,14 @@ export default function ModelPage() {
         loader.load(
           url,
           function (gltf) {
+            //change metiallic
+            gltf.scene.traverse((child) => {
+              if (child.isMesh) {
+                child.material.color = new THREE.Color(0xffffff)
+                child.material.metalness = 0.7
+                child.material.roughness = 0.2
+              }
+            })
             resolve(gltf)
           },
           function (xhr) {
@@ -78,9 +130,166 @@ export default function ModelPage() {
     function render(timestamp, frame) {
       renderer.render(scene, camera)
     }
+
+    bubbleContainer = new THREE.Group()
+
+    //left
+    {
+      //create bubbles using rects
+      //from left to right
+      const bubbleCount = 80
+      const bubbleSize = 0.3
+      const bubbleSpeed = 0.005
+      //list of bubbles
+      const bubbles = []
+      const bubbleOpacity = []
+      const bubbleSpeeds = []
+
+      const geometry = new THREE.PlaneGeometry(bubbleSize, bubbleSize)
+      const tex = new THREE.TextureLoader().load('/bubble.png')
+      tex.encoding = THREE.sRGBEncoding
+      for (let i = 0; i < bubbleCount; i++) {
+        const material = new THREE.MeshBasicMaterial({
+          color: 0xffffff,
+          transparent: true,
+          opacity: 0,
+          map: tex
+        })
+
+        const bubble = new THREE.Mesh(geometry, material)
+        bubble.position.set(
+          Math.random() * 3 - 6,
+          Math.random() * 3.6 - 1.8,
+          Math.random() - 8
+        )
+        //random rotation
+        bubble.rotation.z = Math.random() * Math.PI * 2
+        //bubble.rotation.y = Math.random() * Math.PI * 2
+        bubbles.push(bubble)
+        //init bubble opacity
+        bubbleOpacity.push(0)
+        bubbleSpeeds.push(bubbleSpeed + Math.random() * 0.004)
+        //scene.add(bubble)
+
+        bubbleContainer.add(bubble)
+      }
+
+      const updaetBubbles = function () {
+        for (let i = 0; i < bubbleCount; i++) {
+          const bubble = bubbles[i]
+          bubble.position.x += bubbleSpeeds[i]
+
+          if (bubbleOpacity[i] < 1 && bubble.position.x < -3) {
+            bubbleOpacity[i] += 0.01
+          } else if (bubble.position.x > -3) {
+            bubbleOpacity[i] -= 0.01
+          }
+
+          if (bubble.position.x > -2) {
+            bubble.position.x = -7
+            //reset opacity
+            bubble.material.opacity = 0
+            //new speed
+            bubbleSpeeds[i] = bubbleSpeed + Math.random() * 0.004
+            //new z
+            bubble.position.y = Math.random() * 3.6 - 1.8
+          }
+
+          //opacity
+          bubble.material.opacity = bubbleOpacity[i]
+        }
+      }
+
+      //run 30 times
+      for (let i = 0; i < 1000; i++) {
+        updaetBubbles()
+      }
+
+      setInterval(() => {
+        updaetBubbles()
+      }, 30);
+    }
+
+    //right
+    {
+      //create bubbles using rects
+      //from left to right
+      const bubbleCount = 15
+      const bubbleSize = 1.4
+      const bubbleSpeed = 0.005
+      //list of bubbles
+      const bubbles = []
+      const bubbleOpacity = []
+      const bubbleSpeeds = []
+
+      const geometry = new THREE.PlaneGeometry(bubbleSize, bubbleSize)
+      const tex = new THREE.TextureLoader().load('/bubble2.png')
+      tex.encoding = THREE.sRGBEncoding
+
+      for (let i = 0; i < bubbleCount; i++) {
+        const material = new THREE.MeshBasicMaterial({
+          color: 0xffffff,
+          transparent: true,
+          opacity: 0,
+          map: tex
+        })
+
+        const bubble = new THREE.Mesh(geometry, material)
+        bubble.position.set(
+          Math.random() * 3 + 3,
+          Math.random() * 4 - 2,
+          Math.random() - 8
+        )
+        bubble.rotation.z = Math.random() * Math.PI * 2
+
+        bubbles.push(bubble)
+        //init bubble opacity
+        bubbleOpacity.push(0)
+        bubbleSpeeds.push(bubbleSpeed + Math.random() * 0.004)
+        //scene.add(bubble)
+
+        bubbleContainer.add(bubble)
+      }
+
+      const updaetBubbles = function () {
+        for (let i = 0; i < bubbleCount; i++) {
+          const bubble = bubbles[i]
+          bubble.position.x += bubbleSpeeds[i]
+
+          if (bubbleOpacity[i] < 1 && bubble.position.x < 6) {
+            bubbleOpacity[i] += 0.01
+          } else if (bubble.position.x > 6) {
+            bubbleOpacity[i] -= 0.01
+          }
+
+          if (bubble.position.x > 7) {
+            bubble.position.x = 2
+            //reset opacity
+            bubble.material.opacity = 0
+            //new speed
+            bubbleSpeeds[i] = bubbleSpeed + Math.random() * 0.004
+            //new z
+            bubble.position.y = Math.random() * 4 - 2
+          }
+
+          //opacity
+          bubble.material.opacity = bubbleOpacity[i]
+        }
+      }
+
+      //run 30 times
+      for (let i = 0; i < 1000; i++) {
+        updaetBubbles()
+      }
+
+      setInterval(() => {
+        updaetBubbles()
+      }, 30);
+    }
+
+    scene.add(bubbleContainer)
   }
 
-  /*
   React.useEffect(() => {
     StartThree()
 
@@ -100,25 +309,25 @@ export default function ModelPage() {
         const xangle = (xpercent - 0.5) * Math.PI
         const yangle = (ypercent - 0.5) * Math.PI
 
-        currentgltf.scene.rotation.x = yangle / 100
-        currentgltf.scene.rotation.y = xangle / 100 + Math.PI / 2
+        currentgltf.scene.rotation.x = yangle / 20
+        currentgltf.scene.rotation.y = xangle / 20 + Math.PI / 2
+
+        bubbleContainer.rotation.x = yangle / 20
+        bubbleContainer.rotation.y = xangle / 20
       }
     })
   }, [])
-   */
 
   return (
     <>
       <div style={{ width: '100vw', height: '60vw' }}>
-        <video autoPlay playsInline muted loop style={{ position: 'absolute', width: '100vw', height: '60vw', objectFit: 'cover' }}>
-          <source src={videoTextureURL} type='video/mp4' />
-        </video>
-        <Spline
-          style={{ backgroundColor: 'transparent', width: '100vw', height: '60vw', position: 'absolute',
-            marginTop: '12%',marginLeft: '1.5%'
-           }}
-          scene="https://prod.spline.design/9KEU1wON2uHX2IuF/scene.splinecode"
-        />
+        <div
+          id="three"
+          style={{
+            backgroundColor: 'transparent', width: '100vw', height: '60vw', position: 'absolute'
+          }}
+        >
+        </div>
         <div style={{ position: 'absolute', margin: '6vw' }}>
           <h1
             style={{ fontSize: '4vw' }}
